34da06140dfd1cd0cc8ed6a328f50aed686e3a9e
Bjorn Pettersen <bp@norsktest.no>
2024-09-05 21:22:13 +0200

    prettier

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,38 @@
 <script>
-	import { data } from "./data";
-	import Board from "./Board.svelte";
-	
-	function handleBoardUpdated(newColumnsData) {
-		// if you wanted to update a database or a server, this is where you would do it
-		$data = newColumnsData;
-	}
-</script>
-<style>
-	:global(*) {
-		box-sizing: border-box;
-		margin: 0;
-	}
-	h2, h5 {
-		margin-bottom:0.4em;
-	
-	}
-</style>
-<h2>
-	Board
-</h2>
-<h5>
-	(svelte-dnd-action example)
-</h5>
-<p>
-	This is an exmaple for a board with draggable columns and items. <br />
-	Notice that the scroll position is not maintained if you first scroll down in the first column and then drag it. This is because I wanted to keep this example super clear. To maintain scroll position you will need to save it per column. See <a href="https://svelte.dev/repl/eb2f5988bd2f46488810606c1fb13392?version=3.29.4">this example</a>.
-</p>
-<br/><hr/><br/>
-<Board columns={$data} onFinalUpdate={handleBoardUpdated}/>
+    import { data } from './data'
+    import Board from './Board.svelte'
 
+    function handleBoardUpdated(newColumnsData) {
+        // if you wanted to update a database or a server, this is where you would do it
+        $data = newColumnsData
+    }
+</script>
+
+<h2>Board</h2>
+<h5>(svelte-dnd-action example)</h5>
+<p>
+    This is an exmaple for a board with draggable columns and items. <br />
+    Notice that the scroll position is not maintained if you first scroll down in
+    the first column and then drag it. This is because I wanted to keep this example
+    super clear. To maintain scroll position you will need to save it per column.
+    See
+    <a
+        href="https://svelte.dev/repl/eb2f5988bd2f46488810606c1fb13392?version=3.29.4"
+        >this example</a
+    >.
+</p>
+<br />
+<hr />
+<br />
+<Board columns={$data} onFinalUpdate={handleBoardUpdated} />
+
+<style>
+    :global(*) {
+        box-sizing: border-box;
+        margin: 0;
+    }
+    h2,
+    h5 {
+        margin-bottom: 0.4em;
+    }
+</style>
--- src/routes/Board.svelte
+++ src/routes/Board.svelte
@@ -1,28 +1,46 @@
 <script>
-	import { flip } from 'svelte/animate';
-  import { dndzone } from 'svelte-dnd-action';
-	import Column from "./Column.svelte";
-	
-	const flipDurationMs = 300;
-	
-	// will be called any time a card or a column gets dropped to update the parent data
-  /** @type {{columns: any, onFinalUpdate: any}} */
-  let { columns = $bindable(), onFinalUpdate } = $props();
-	
-	let isDraggingFolder = $state(false);
- 
-  function handleDndConsiderColumns(e) {
-    columns = e.detail.items;
-  }
-  function handleDndFinalizeColumns(e) {
-    onFinalUpdate(e.detail.items);
-  }
- 	function handleItemFinalize(columnIdx, newItems) {
-		columns[columnIdx].items = newItems;
-		onFinalUpdate([...columns]);
-		isDraggingFolder = false;
-	}
+    import { flip } from 'svelte/animate'
+    import { dndzone } from 'svelte-dnd-action'
+    import Column from './Column.svelte'
+
+    const flipDurationMs = 300
+
+    // will be called any time a card or a column gets dropped to update the parent data
+    /** @type {{columns: any, onFinalUpdate: any}} */
+    let { columns = $bindable(), onFinalUpdate } = $props()
+
+    let isDraggingFolder = $state(false)
+
+    function handleDndConsiderColumns(e) {
+        columns = e.detail.items
+    }
+    function handleDndFinalizeColumns(e) {
+        onFinalUpdate(e.detail.items)
+    }
+    function handleItemFinalize(columnIdx, newItems) {
+        columns[columnIdx].items = newItems
+        onFinalUpdate([...columns])
+        isDraggingFolder = false
+    }
 </script>
+
+<section
+    class="board"
+    use:dndzone={{ items: columns, flipDurationMs, type: 'column' }}
+    onconsider={handleDndConsiderColumns}
+    onfinalize={handleDndFinalizeColumns}>
+    {#each columns as { id, name, items }, idx (id)}
+        <div class="column" animate:flip={{ duration: flipDurationMs }}>
+            <Column
+                {name}
+                {items}
+                {isDraggingFolder}
+                onFolderDragStart={() => (isDraggingFolder = true)}
+                onDrop={newItems => handleItemFinalize(idx, newItems)} />
+        </div>
+    {/each}
+</section>
+
 <style>
     .board {
         height: 60vh;
@@ -37,18 +55,7 @@
         margin: 1em;
         float: left;
         border: 1px solid #333333;
-				background-color: white;
-			  color: black;
+        background-color: white;
+        color: black;
     }
 </style>
-
-<section class="board" 
-	use:dndzone={{items:columns, flipDurationMs, type:'column'}} 
-	onconsider={handleDndConsiderColumns} 
-	onfinalize={handleDndFinalizeColumns}>
-    {#each columns as {id, name,items}, idx (id)}
-  		<div class="column"animate:flip="{{duration: flipDurationMs}}" >    
-				<Column name={name} items={items} isDraggingFolder={isDraggingFolder} onFolderDragStart={() => (isDraggingFolder = true)} onDrop={(newItems) => handleItemFinalize(idx, newItems)} />
-			</div>
-    {/each}
-</section>
